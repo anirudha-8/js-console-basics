3698c1a0df51928518fd2dcb218a4bd8ed477783
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 11:09:17 +0530

    feat: add solution for 9-Problem

---
+++ 9-Problem.js
@@ -0,0 +1,9 @@
+// 9-Problem statement - Create a variable holding your **age** and check whether it’s **greater than 18**.
+
+const age = 23;
+
+// vintage way
+if (age > 18) console.log("greater that 18");
+
+// modern way
+console.log(age > 18 ? "greater that 18" : "");
