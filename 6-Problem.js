72e2fe1fe5cdf813ad743a8c20e88efc73d34b66
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 10:59:54 +0530

    feat: add solution for 6-Problem

---
+++ 6-Problem.js
@@ -0,0 +1,7 @@
+// 6-Problem statement - Use `console.error()` to simulate an **error message**.
+
+console.error(`🚨 404 - This is sample error message!`);
+
+console.log("");
+
+console.log(new Error("This is another sample error message!"));
