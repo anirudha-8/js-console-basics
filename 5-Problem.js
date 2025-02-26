15f3626c215885264877ebcffb01f4432dd9d128
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 10:53:56 +0530

    feat: add solution for 5-Problem

---
+++ 5-Problem.js
@@ -0,0 +1,10 @@
+// 5-Problem statement - Track the value of a variable by logging it **before and after updating**.
+
+let number = 8;
+
+console.log("--------- Before updating the value of variable ---------");
+console.log(`Value of number is: ${number}`);
+
+console.log("\n--------- After updating the value of variable ---------");
+number = 88;
+console.log(`Value of number is: ${number}`);
