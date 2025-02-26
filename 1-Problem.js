5d0fc396322fe045999127f825744ba05a7dd332
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 10:31:30 +0530

    feat: add solution for 1-Problem

---
+++ 1-Problem.js
@@ -0,0 +1,7 @@
+// 1-Problem statement - Log your name and favorite hobby to the console
+
+const myName = "Anirudha Bele";
+
+const favoriteHobby = "Watching Anime";
+
+console.log(`My name is ${myName} and my favorite hobby is - ${favoriteHobby}`);
