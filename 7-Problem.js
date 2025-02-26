5f6cb00b5180b32de3f42c150d4c80e25e3dde25
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 11:03:22 +0530

    feat: add solution for 7-Problem

---
+++ 7-Problem.js
@@ -0,0 +1,9 @@
+// 7-Problem statement - Log the **square** of the number `12` to the console.
+
+// 1st vintage way
+// const squareOfTwelve = Math.pow(12, 2);
+
+// 2nd modern way
+const squareOfTwelve = 12 ** 2;
+
+console.log(`Square of 12: ${squareOfTwelve}`);
