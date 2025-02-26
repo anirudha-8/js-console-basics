ec730b5a86e7fa29811cc15f37b085f6ba7526ec
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 10:49:02 +0530

    feat: add solution for 4-Problem

---
+++ 4-Problem.js
@@ -0,0 +1,7 @@
+// 4-Problem.js statement - Create two variables for **first and last name**, concatenate them, and log the result.
+
+const firstName = "Anirudha";
+
+const lastName = "Bele";
+
+console.log(`Full name is: ${firstName} ${lastName}`);
