01f955cec871c55a7e3ce81248d530e554d6d658
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-26 11:06:33 +0530

    feat: add solution for 8-Problem

---
+++ 8-Problem.js
@@ -0,0 +1,7 @@
+// 8-Problem statement - Print the **type** of a variable holding the value `true`.
+
+const isValid = true;
+
+console.log(
+	`The type of isValid variable value (${isValid}) is: ${typeof isValid}`
+);
